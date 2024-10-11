f5abd8f8b14b4dda8b400d85115944bdbf62d001
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-10-11 17:46:46 +0400

    add variable declaration, casting and function types. Lecture 2

--- src/veryfirst/index.ts
+++ src/veryfirst/index.ts
@@ -1,32 +1,32 @@
-/**
- * Create a promise that resolves after some time
- * @param n number of milliseconds before promise resolves
- */
+// /**
+//  * Create a promise that resolves after some time
+//  * @param n number of milliseconds before promise resolves
+//  */
 
-function timeout(n: number) {
-  return new Promise((res) => setTimeout(res, n));
-}
+// function timeout(n: number) {
+//   return new Promise((res) => setTimeout(res, n));
+// }
 
-/**
- * Add three numbers
- * @param a first number
- * @param b second
- */
-export async function addNumbers(a: number, b: number) {
-  await timeout(500);
-  return a + b;
-}
+// /**
+//  * Add three numbers
+//  * @param a first number
+//  * @param b second
+//  */
+// export async function addNumbers(a: number, b: number) {
+//   await timeout(500);
+//   return a + b;
+// }
 
-// this exists es2022
-class Foo {
-  static #bar = 3;
-  static getValue() {
-    return Foo.#bar;
-  }
-}
+// // this exists es2022
+// class Foo {
+//   static #bar = 3;
+//   static getValue() {
+//     return Foo.#bar;
+//   }
+// }
 
-//== Run the program ==//
-// it exists es 2017
-(async () => {
-  console.log(await addNumbers(Foo.getValue(), 4));
-})();
+// //== Run the program ==//
+// // it exists es 2017
+// (async () => {
+//   console.log(await addNumbers(Foo.getValue(), 4));
+// })();
